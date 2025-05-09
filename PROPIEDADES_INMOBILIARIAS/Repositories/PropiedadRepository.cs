5779c47178804caf71f4b92ee8465fa0ba0668c5
Deiiivy <deivypr28@gmail.com>
2025-05-09 00:02:40 -0500

    se termina el metodo para agregar un producto

--- PROPIEDADES_INMOBILIARIAS/Repositories/PropiedadRepository.cs
+++ PROPIEDADES_INMOBILIARIAS/Repositories/PropiedadRepository.cs
@@ -19,7 +19,17 @@ namespace PROPIEDADES_INMOBILIARIAS.Repositories
 
         public void AgregarPropiedad(Propiedad propiedad)
         {
-           
+            string query = "INSERT INTO Propiedades (Direccion, Tipo, Superficie, Precio, Estado) " +
+                 "VALUES (@direccion, @tipo, @superficie, @precio, @estado)";
+
+            SqlCommand cmd = new SqlCommand(query, conexion);
+            cmd.Parameters.AddWithValue("@direccion", propiedad.Direccion);
+            cmd.Parameters.AddWithValue("@tipo", propiedad.Tipo);
+            cmd.Parameters.AddWithValue("@superficie", propiedad.Superficie);
+            cmd.Parameters.AddWithValue("@precio", propiedad.Precio);
+            cmd.Parameters.AddWithValue("@estado", propiedad.Estado);
+
+            cmd.ExecuteNonQuery();
         }
 
         public void ActualizarPropiedad(Propiedad propiedad)
